b2eaa2fe0fac21df31f1ff0bc8b8da530c2453ae
Said <shikhsaidov@mail.ru>
2022-11-17 00:03:30 +0300

    Сделал авторизацию и регистрацию через Firebase

--- app/src/main/java/com/example/company_application/MainActivity.kt
+++ app/src/main/java/com/example/company_application/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.company_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openRegistrationScreen()
+    }
+
+    private fun openRegistrationScreen(){
+
+        // Choose authentication providers
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+        }
     }
 }
