46d0d494b57f550fc82e8b674b278ba7f19685d7
Said <shikhsaidov@mail.ru>
2022-11-21 23:22:24 +0300

    Добавил меню

--- app/src/main/java/com/example/company_application/MainActivity.kt
+++ app/src/main/java/com/example/company_application/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.company_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
@@ -24,6 +29,40 @@ class MainActivity : AppCompatActivity() {
         openRegistrationScreen()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        val user = FirebaseAuth.getInstance().currentUser
+        val login = user?.email.toString()
+
+        Log.i("login", login)
+
+        //проверка, какой пользователь входит
+        if (login[0] == '1'){
+            Log.i("login", "login_1")
+        }else{
+            inflater.inflate(R.menu.menu, menu)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.personal -> {
+                startActivity(Intent(this, PersonalActivity::class.java))
+                true
+            }
+            R.id.projects -> {
+                startActivity(Intent(this, ProjectsActivity::class.java))
+                true
+            }
+            R.id.reports -> {
+                startActivity(Intent(this, ReportsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun openRegistrationScreen(){
 
         // Choose authentication providers
@@ -41,7 +80,6 @@ class MainActivity : AppCompatActivity() {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
             // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
         }
     }
 }
