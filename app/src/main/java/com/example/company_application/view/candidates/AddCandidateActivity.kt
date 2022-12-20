880e93f5e9204e29500029c87577fdbfadba2735
Karambasss <m.papa.09@mail.ru>
2022-12-20 23:50:11 +0300

    Добавил много нового...

---
+++ app/src/main/java/com/example/company_application/view/candidates/AddCandidateActivity.kt
@@ -0,0 +1,18 @@
+package com.example.company_application.view.candidates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.company_application.R
+import kotlinx.android.synthetic.main.activity_add_candidate.*
+
+class AddCandidateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_candidate)
+
+        add_activity_candidate_saveBtn.setOnClickListener{
+            finish()
+        }
+
+    }
+}
